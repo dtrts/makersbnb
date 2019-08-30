875482dbd1a69fe7650f2fe48023ad5725876154
dtrts <djt210@gmail.com>
2019-08-30 11:22:49 +0100

    RED basic bookings

---
+++ spec/features/basic_bookings_spec.rb
@@ -0,0 +1,35 @@
+feature 'basic bookings' do
+  scenario 'unable to book as a normal user' do
+    visit('/')
+    first_booking = find(:xpath, '//div[@class="listing"][1]')
+    expect(first_booking).to have_button('Save', disabled: true)
+  end
+
+  scenario 'able to click booking button signed in' do
+    visit('/')
+    click_button('Log In')
+    fill_in('Username:', with: 'admin')
+    fill_in('Password:', with: 'password')
+    click_button('Submit')
+    first_booking = find(:xpath, '//div[@class="listing"][1]')
+    expect(first_booking).to have_button('Book', disabled: false)
+  end
+
+
+  scenario 'can make a booking' do
+    visit('/')
+    click_button('Log In')
+    fill_in('Username:', with: 'admin')
+    fill_in('Password:', with: 'password')
+    click_button('Submit')
+    within(:xpath, '//div[@class="listing"][1]') do
+      click_button('Book')
+    end
+    expect(page).to have_content("Booking Confirmed")
+    click_button('Return Home')
+    first_booking = find(:xpath, '//div[@class="listing"][1]')
+    expect(first_booking).not_to have_button('Book')
+    expect(first_booking).to have_content('Unavailable')
+  end
+
+end
